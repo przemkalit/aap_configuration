ed15e7528e6741b40626db87ee34bd4b4d2d075f
Ivan Aragonés Muniesa <iaragone@redhat.com>
2022-03-10 09:53:49 +0100

    Update to controller_object_diff lookup plugin to better handle other objetcs. lintering fixes. add_workflow_schema fixes. fixed examples inputs

--- plugins/lookup/controller_object_diff.py
+++ plugins/lookup/controller_object_diff.py
@@ -110,9 +110,22 @@ class LookupModule(LookupBase):
                 raise AnsibleLookupError("Unable to find items in api_list")
             return [api_list]
 
-        # Set Keys to keep for each list.
-        keys_to_keep = ["name", "organization"]
-        api_keys_to_keep = ["name", "summary_fields"]
+        # Set Keys to keep for each list. Depending on type
+        if api_list[0]["type"] == "organization":
+            keys_to_keep = ["name"]
+            api_keys_to_keep = ["name"]
+        elif api_list[0]["type"] == "user":
+            keys_to_keep = ["username"]
+            api_keys_to_keep = ["username"]
+        elif api_list[0]["type"] == "workflow_job_template_node":
+            keys_to_keep = ["workflow_job_template", "unified_job_template", "identifier"]
+            api_keys_to_keep = ["identifier", "summary_fields"]
+        elif api_list[0]["type"] == "group" or api_list[0]["type"] == "host":
+            keys_to_keep = ["name", "inventory"]
+            api_keys_to_keep = ["name", "summary_fields"]
+        else:
+            keys_to_keep = ["name", "organization"]
+            api_keys_to_keep = ["name", "summary_fields"]
 
         # Depending on type, keep additional keys
         if api_list[0]["type"] == "credential":
@@ -136,10 +149,28 @@ class LookupModule(LookupBase):
         compare_list_reduced = [{key: item[key] for key in keys_to_keep} for item in compare_list]
         api_list_reduced = [{key: item[key] for key in api_keys_to_keep} for item in api_list]
 
-        # Convert summary field name into org name
-        for item in api_list_reduced:
-            item.update({"organization": item["summary_fields"]["organization"]["name"]})
-            item.pop("summary_fields")
+        # Convert summary field name into org name Only if not type organization
+        if api_list[0]["type"] == "group" or api_list[0]["type"] == "host":
+            for item in api_list_reduced:
+                item.update({"inventory": item["summary_fields"]["inventory"]["name"]})
+                item.pop("summary_fields")
+        elif api_list[0]["type"] == "credential":
+            for item in api_list_reduced:
+                item.update({"organization": item["summary_fields"]["organization"]["name"]})
+                item.update({"credential_type": item["summary_fields"]["credential_type"]["name"]})
+                item.pop("summary_fields")
+        elif api_list[0]["type"] == "workflow_job_template_node":
+            for item in api_list_reduced:
+                item.update({"unified_job_template": item["summary_fields"]["unified_job_template"]["name"]})
+                item.update({"workflow_job_template": item["summary_fields"]["workflow_job_template"]["name"]})
+                item.pop("summary_fields")
+        elif api_list[0]["type"] != "organization" and api_list[0]["type"] != "user":
+            for item in api_list_reduced:
+                item.update({"organization": item["summary_fields"]["organization"]["name"]})
+                item.pop("summary_fields")
+
+        self.display.warning("compare_list_reduced: {0}".format(compare_list_reduced))
+        self.display.warning("api_list_reduced: {0}".format(api_list_reduced))
 
         # Find difference between lists
         difference = [i for i in api_list_reduced if i not in compare_list_reduced]
